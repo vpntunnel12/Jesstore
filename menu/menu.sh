ba0a19c8efbadedca7db4efa5337fe080ef31826
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2024-09-28 13:57:11 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -1,50 +1,81 @@
 #!/bin/bash
+
 BURIQ () {
-curl -sS https://raw.githubusercontent.com/scriswan/premiumsc/main/register > /root/tmp
-data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-for user in "${data[@]}"
-do
-exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-d1=(`date -d "$exp" +%s`)
-d2=(`date -d "$biji" +%s`)
-exp2=$(( (d1 - d2) / 86400 ))
-if [[ "$exp2" -le "0" ]]; then
-echo $user > /etc/.$user.ini
-else
-rm -f /etc/.$user.ini > /dev/null 2>&1
-fi
-done
-rm -f /root/tmp
+    # Mengunduh daftar register dari URL baru dan menyimpannya ke dalam file sementara
+    curl -sS https://raw.githubusercontent.com/scriswan/premiumsc/main/register > /root/tmp
+    
+    # Mendapatkan IP pengguna saat ini
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    
+    # Mencari entri yang memiliki IP yang sama dengan pengguna saat ini
+    entry=$(grep -E " $MYIP" /root/tmp)
+    
+    # Jika entri ditemukan, proses tanggal kedaluwarsa
+    if [[ -n "$entry" ]]; then
+        # Mengambil nama pengguna dan tanggal kedaluwarsa dari entri tersebut
+        user=$(echo "$entry" | awk '{print $2}')
+        exp=$(echo "$entry" | awk '{print $3}')
+        
+        # Mengonversi tanggal kedaluwarsa dan tanggal hari ini ke detik sejak epoch
+        d1=$(date -d "$exp" +%s)
+        d2=$(date +%s) # Hari ini
+        
+        # Menghitung selisih hari antara hari ini dan tanggal kedaluwarsa
+        exp2=$(( (d1 - d2) / 86400 ))
+        
+        # Jika waktu habis (expired)
+        if [[ "$exp2" -le "0" ]]; then
+            echo $user > /etc/.$user.ini
+        else
+            rm -f /etc/.$user.ini > /dev/null 2>&1
+        fi
+    fi
+
+    # Menghapus file sementara
+    rm -f /root/tmp
 }
+
+# Mendapatkan IP pengguna saat ini
 MYIP=$(curl -sS ipv4.icanhazip.com)
+
+# Mendapatkan nama berdasarkan IP yang sesuai di file register
 Name=$(curl -sS https://raw.githubusercontent.com/scriswan/premiumsc/main/register | grep $MYIP | awk '{print $2}')
+
+# Menyimpan nama ke file .ini untuk pemeriksaan izin
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
+
 Bloman () {
-if [ -f "/etc/.$Name.ini" ]; then
-CekTwo=$(cat /etc/.$Name.ini)
-if [ "$CekOne" = "$CekTwo" ]; then
-res="Expired"
-fi
-else
-res="Permission Accepted..."
-fi
+    # Memeriksa apakah file izin untuk user ada
+    if [ -f "/etc/.$Name.ini" ]; then
+        CekTwo=$(cat /etc/.$Name.ini)
+        
+        # Memeriksa apakah nama pada file izin cocok
+        if [ "$CekOne" = "$CekTwo" ]; then
+            res="Expired"
+        fi
+    else
+        res="Permission Accepted..."
+    fi
 }
+
 PERMISSION () {
-MYIP=$(curl -sS ipv4.icanhazip.com)
-IZIN=$(curl -sS https://raw.githubusercontent.com/scriswan/premiumsc/main/register | awk '{print $4}' | grep $MYIP)
-if [ "$MYIP" = "$IZIN" ]; then
-Bloman
-else
-res="Permission Denied!"
-fi
-BURIQ
+    # Mendapatkan IP pengguna
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    
+    # Memeriksa apakah IP ada di daftar register
+    IZIN=$(curl -sS https://raw.githubusercontent.com/scriswan/premiumsc/main/register | awk '{print $4}' | grep $MYIP)
+    
+    if [ "$MYIP" = "$IZIN" ]; then
+        Bloman
+    else
+        res="Permission Denied!"
+    fi
+    
+    BURIQ
 }
-red='\e[1;31m'
-green='\e[1;32m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+# Menjalankan fungsi PERMISSION untuk memulai
 PERMISSION
 if [ "$res" = "Expired" ]; then
 Exp="\e[36mExpired\033[0m"
