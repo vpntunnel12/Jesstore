b0a4ae6c7d9cd840978073a9b802323bf30846ed
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2024-10-02 00:32:19 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -261,7 +261,7 @@ IPVPS=$(curl -s ipinfo.io/ip )
 clear
 echo -e "$(figlet   "")" | lolcat
 echo -e "${BICyan} ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${NC}"
-echo -e "${BICyan} │       ${BIWhite}${UWhite}SCRIPT PREMIUM KEYRIS VPN WA : 085888801241${NC}"
+echo -e "${BICyan} │       ${BIWhite}${UWhite}🔹SELAMAT MENGUNAKAN SC KEYRIS VPNSTORE🔹${NC}"
 echo -e "${BICyan} ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬${NC}"
 echo -e "${BICyan} │  ${BICyan}OS        :  ${BIYellow}$( cat /etc/os-release | grep -w PRETTY_NAME | sed 's/PRETTY_NAME//g' | sed 's/=//g' | sed 's/"//g' )${NC}"
 echo -e "${BICyan} │  ${BICyan}NS        :  ${BIYellow}$(cat /root/nsdomain)${NC}"
