297ffcfe808f7577727cc967fa949e9364329f11
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2024-11-26 18:44:38 +0700

    Update bot-bansos.sh

--- bot/bot-bansos.sh
+++ bot/bot-bansos.sh
@@ -32,11 +32,14 @@ clear
 
 
 echo ""
+echo ""
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[44;97;1m         ADD BOT CREATE PUBLIC          $NC"
+echo -e " \e[44;97;1m        ADD BOT CREATE PRIVATE          $NC"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 read -e -p "Bot Token : " bottoken
+read -e -p "ID Telegram :" admin
 echo -e BOT_TOKEN='"'$bottoken'"' >> /usr/bin/kyt/var.txt
+echo -e ADMIN='"'$admin'"' >> /usr/bin/kyt/var.txt
 echo -e DOMAIN='"'$domain'"' >> /usr/bin/kyt/var.txt
 echo -e PUB='"'$PUB'"' >> /usr/bin/kyt/var.txt
 echo -e HOST='"'$NS'"' >> /usr/bin/kyt/var.txt
