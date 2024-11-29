ba6633f4d2debf2e417f8695712830a614a6f841
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2024-11-29 12:37:41 +0700

    Update add-bot.sh

--- bot/add-bot.sh
+++ bot/add-bot.sh
@@ -35,7 +35,7 @@ echo ""
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e " \e[44;97;1m        ADD BOT CREATE PRIVATE          $NC"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-read -e -p " bottoken
+read -e -p "Silahkan enter lalu masukan Id peribadi " bottoken
 read -e -p "ID Telegram :" admin
 echo -e BOT_TOKEN='"'$7492113299:AAHywOxMwpR6EwlSy_HNoNiotPuXWeN_YCI'"' >> /usr/bin/kyt/var.txt
 echo -e ADMIN='"'$admin'"' >> /usr/bin/kyt/var.txt
