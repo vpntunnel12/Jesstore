2fde0aca6f69fe5942cb460314fd49e335867b5c
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2024-12-31 22:33:49 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -279,7 +279,7 @@ IPVPS=$(curl -s ipinfo.io/ip )
 clear
 echo -e "$(figlet   "")" | lolcat
 echo -e "\e[33m║━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\e[33m║${NC}${BIYellow}   \e[41;97;1m             SCRIPT BY RISWAN STORE             \033[0m\e[38;5;196m${NC}"
+echo -e "\e[33m║${NC}${BIYellow}   \e[41;97;1m            ◥꧁SCRIPT BY RISWAN STORE꧂◤             \033[0m\e[38;5;196m${NC}"
 echo -e "\e[33m║━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "\e[33m║${NC} ${BIRed}OS        ➤ ${BIWhite}$(cat /etc/os-release | grep -w PRETTY_NAME | sed 's/PRETTY_NAME=//g' | sed 's/"//g')${NC}"
 echo -e "\e[33m║${NC} ${BIRed}UPTIME    ➤ ${BIWhite}$uptime_info$(uptime -p | sed 's/up //')${NC}"  
