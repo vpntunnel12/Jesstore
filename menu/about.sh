4f5ddfbee6c181a737fa32fe2ca0c1a3b2cca4bd
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2025-01-03 02:02:16 +0700

    Update about.sh

--- menu/about.sh
+++ menu/about.sh
@@ -29,7 +29,7 @@ echo "   - Dflate               : [ON]"  | tee -a log-install.txt
 echo "   - IPtables             : [ON]"  | tee -a log-install.txt
 echo "   - Auto-Reboot          : [ON]"  | tee -a log-install.txt
 echo "   - IPv6                 : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On        : $aureb="18:00" $WIB GMT+7" | tee -a log-install.txt
+echo "   - Autoreboot On        : $aureb:00 $gg GMT +7" | tee -a log-install.txt
 echo "   - AutoKill Multi Login User" | tee -a log-install.txt
 echo "   - Auto Delete Expired Account" | tee -a log-install.txt
 echo "   - Fully automatic script" | tee -a log-install.txt
