481beac434b4862152b5128caa0a504e805c0303
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2025-01-02 16:05:59 +0700

    Update menu-vmess.sh

--- menu/menu-vmess.sh
+++ menu/menu-vmess.sh
@@ -106,16 +106,15 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 
 clear
-echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
-echo -e "     ${BIWhite}${UWhite}MENU VMESS ${NC}"
-echo -e ""
-echo -e "     ${BICyan}[${BIWhite}01${BICyan}] Create Account XRAY Vmess Websocket "
-echo -e "     ${BICyan}[${BIWhite}02${BICyan}] Trial Account XRAY Vmess     "
-echo -e "     ${BICyan}[${BIWhite}03${BICyan}] Extending Account XRAY Vmess Active "
-echo -e "     ${BICyan}[${BIWhite}04${BICyan}] Delete Account XRAY Vmess Websocket  "
-echo -e "     ${BICyan}[${BIWhite}05${BICyan}] Check User Login XRAY Vmess     "
-
-echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
+cho -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "              ${BIWhite}${UWhite}MENU VMESS XRAY${NC}"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "  ${BICyan}[${BIWhite}01${BICyan}] CREATE ACCOUNT VMESS"
+echo -e "  ${BICyan}[${BIWhite}02${BICyan}] TRIAL ACCOUNT VMESS"
+echo -e "  ${BICyan}[${BIWhite}03${BICyan}] ACCOUNT VMESS AKTIP"
+echo -e "  ${BICyan}[${BIWhite}04${BICyan}] DELETE USER VMESS"
+echo -e "  ${BICyan}[${BIWhite}05${BICyan}] CEK LOGIN VMESS"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "     ${BIYellow}Press x or [ Ctrl+C ] • To-${BIWhite}Exit${NC}"
 echo ""
 read -p " Select menu :  "  opt
