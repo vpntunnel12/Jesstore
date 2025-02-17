906030c91a61b86c19b23a642a141a39424e3aac
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2025-02-17 16:08:10 +0700

    Update renew-ws.sh

--- xray/renew-ws.sh
+++ xray/renew-ws.sh
@@ -25,39 +25,44 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
-	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		clear
-		echo ""
-		echo "You have no existing clients!"
-		exit 1
-	fi
+if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+    clear
+    echo ""
+    echo "You have no existing clients!"
+    exit 1
+fi
 
-	clear
-	echo ""
-	echo "Select the existing client you want to renew"
-	echo " Press CTRL+C to return"
-	echo -e "==============================="
-	grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
-	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
-		if [[ ${CLIENT_NUMBER} == '1' ]]; then
-			read -rp "Select one client [1]: " CLIENT_NUMBER
-		else
-			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
-		fi
-	done
-read -p "Expired (Days): " masaaktif
-user=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+clear
+echo ""
+echo "Select the existing client you want to renew"
+echo " Press CTRL+C to return"
+echo -e "==============================="
+# Tampilkan semua username unik dan urutannya
+grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2 | sort | uniq | nl -s ') '
+
+# Ambil input nomor klien yang ingin diperpanjang
+until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+    read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+done
+
+# Ambil username dan tanggal kedaluwarsa dari klien yang dipilih
+user=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2 | sort | uniq | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+
+# Proses perpanjangan
 now=$(date +%Y-%m-%d)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
+read -p "Expired (Days): " masaaktif
 exp3=$(($exp2 + $masaaktif))
 exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/config.json
+
+# Update file konfigurasi dan restart layanan
 sed -i "s/### $user $exp/### $user $exp4/g" /etc/xray/config.json
 systemctl restart xray.service
 service cron restart
+
 clear
 echo ""
 echo "==============================="
@@ -69,4 +74,3 @@ echo "==============================="
 echo "Script Mod By Riswanvpn"
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
-
