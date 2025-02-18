3c52cc88958be9f9870635d53abfe02bec766d11
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2025-02-18 10:55:49 +0700

    Update renew-ws.sh

--- xray/renew-ws.sh
+++ xray/renew-ws.sh
@@ -11,13 +11,11 @@
 RED='\033[0;31m'
 NC='\033[0m'
 GREEN='\033[0;32m'
-ORANGE='\033[0;33m'    # Warna oranye
+ORANGE='\033[0;33m'
 BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
-YELLOW='\033[0;33m'   # Mengganti warna menjadi kuning
-
 # ==========================================
 # Getting
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
@@ -30,29 +28,25 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
     clear
     echo ""
-    echo "Anda tidak memiliki klien yang ada!"
+    echo "You have no existing clients!"
     exit 1
 fi
 
 clear
 echo ""
-echo "Pilih klien yang ingin Anda perpanjang"
-echo " Tekan CTRL+C untuk kembali"
+echo "Select the existing client you want to renew"
+echo " Press CTRL+C to return"
 echo -e "==============================="
-# Menampilkan semua username dan tanggal expired-nya dengan nomor urut
-grep -E "^### " "/etc/xray/config.json" | nl -s ') ' | while read line; do
-    user=$(echo $line | cut -d ' ' -f 2)
-    exp=$(echo $line | cut -d ' ' -f 3)
-    echo "$user - Expired: $exp"
-done
+# Tampilkan semua username unik dan urutannya
+grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2 | sort | uniq | nl -s ') '
 
 # Ambil input nomor klien yang ingin diperpanjang
 until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
-    read -rp "Pilih salah satu klien [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+    read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
 done
 
 # Ambil username dan tanggal kedaluwarsa dari klien yang dipilih
-user=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2 | sort | uniq | sed -n "${CLIENT_NUMBER}"p)
+user=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq | sed -n "${CLIENT_NUMBER}"p)
 exp=$(grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 
 # Proses perpanjangan
@@ -60,7 +54,7 @@ now=$(date +%Y-%m-%d)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
-read -p "Expired (Hari): " masaaktif
+read -p "Expired (Days): " masaaktif
 exp3=$(($exp2 + $masaaktif))
 exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
 
@@ -72,11 +66,11 @@ service cron restart
 clear
 echo ""
 echo "==============================="
-echo "  Akun Xray/Vmess Diperpanjang  "
+echo "  Xray/Vmess Account Renewed  "
 echo "==============================="
-echo "Username      : $user"
-echo -e "Tanggal Expired : ${YELLOW}$exp4${NC}"   # Mengganti label Tanggal Expired dengan warna kuning
+echo "Username  : $user"
+echo "Expired   : $exp4"
 echo "==============================="
-echo "Skrip Mod Oleh Riswanvpn"
-read -n 1 -s -r -p "Tekan tombol apapun untuk kembali ke menu"
+echo "Script Mod By Riswanvpn"
+read -n 1 -s -r -p "Press any key to back on menu"
 menu
