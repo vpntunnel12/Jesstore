1fde58ecf904f008a414c63e79af1663fa3317d1
riswandii-lop <Litastore41@gmail.com>
2025-03-12 20:44:20 +0700

    Update menu-bot.sh

--- menu/menu-bot.sh
+++ menu/menu-bot.sh
@@ -7,7 +7,7 @@ RED="\033[0;31m"
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/scriswan/premiumsc/main/register | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/register | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
         echo "Permission Accepted"
         res="Permission Accepted"
