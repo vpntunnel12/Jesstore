ef1e5f9e8af970002803f417ee887a131cd9fd63
riswandii-lop <Litastore41@gmail.com>
2025-03-12 20:50:32 +0700

    Update add-bot.sh

--- bot/add-bot.sh
+++ bot/add-bot.sh
@@ -19,13 +19,13 @@ apt update && apt upgrade
 apt install neofetch -y
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/bot.zip
+wget https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/kyt.zip
+wget https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 clear
@@ -68,80 +68,79 @@ echo -e " \033[1;97;41m     MENDOWNLOAD ASSET TAMBAHAN......    \033[0m"
 
 
 
-
-wget -q -O /usr/bin/addnoobz "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/addnoobz.sh" && chmod +x /usr/bin/addnoobz
-
+wget -q -O /usr/bin/addnoobz "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/addnoobz.sh" && chmod +x /usr/bin/addnoobz
 
 
 
 
 
-wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/add-vless.sh" && chmod +x /usr/bin/add-vless
 
-wget -q -O /usr/bin/addtr "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/addtr.sh" && chmod +x /usr/bin/addtr
+wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/add-vless.sh" && chmod +x /usr/bin/add-vless
 
-
-wget -q -O /usr/bin/addws "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/addws.sh" && chmod +x /usr/bin/addws
+wget -q -O /usr/bin/addtr "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/addtr.sh" && chmod +x /usr/bin/addtr
 
 
-wget -q -O /usr/bin/addss "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/addss.sh" && chmod +x /usr/bin/addss
-
-wget -q -O /usr/bin/cek-ssh "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
+wget -q -O /usr/bin/addws "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/addws.sh" && chmod +x /usr/bin/addws
 
 
+wget -q -O /usr/bin/addss "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/addss.sh" && chmod +x /usr/bin/addss
 
-wget -q -O /usr/bin/cek-ss "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/cek-ss.sh" && chmod +x /usr/bin/cek-ss
+wget -q -O /usr/bin/cek-ssh "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
 
 
 
-
-wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/cek-tr.sh" && chmod +x /usr/bin/cek-tr
-
-wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/cek-vless.sh" && chmod +x /usr/bin/cek-vless
-
-wget -q -O /usr/bin/cek-ws "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/cek-ws.sh" && chmod +x /usr/bin/cek-ws
-
-wget -q -O /usr/bin/del-vless "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/del-vless.sh" && chmod +x /usr/bin/del-vless
+wget -q -O /usr/bin/cek-ss "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/cek-ss.sh" && chmod +x /usr/bin/cek-ss
 
 
 
 
-wget -q -O /usr/bin/cek-noobz "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
+wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/cek-tr.sh" && chmod +x /usr/bin/cek-tr
+
+wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/cek-vless.sh" && chmod +x /usr/bin/cek-vless
+
+wget -q -O /usr/bin/cek-ws "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/cek-ws.sh" && chmod +x /usr/bin/cek-ws
+
+wget -q -O /usr/bin/del-vless "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/del-vless.sh" && chmod +x /usr/bin/del-vless
 
 
 
-wget -q -O /usr/bin/deltr "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/deltr.sh" && chmod +x /usr/bin/deltr
 
-wget -q -O /usr/bin/delws "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/delws.sh" && chmod +x /usr/bin/delws
-
-
-
-wget -q -O /usr/bin/delss "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/delss.sh" && chmod +x /usr/bin/delss
+wget -q -O /usr/bin/cek-noobz "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
 
 
 
-wget -q -O /usr/bin/renew-ss "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/renew-ss.sh" && chmod +x /usr/bin/renew-ss
+wget -q -O /usr/bin/deltr "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/deltr.sh" && chmod +x /usr/bin/deltr
 
-
-wget -q -O /usr/bin/renewtr "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/renewtr.sh" && chmod +x /usr/bin/renewtr
-
-
-wget -q -O /usr/bin/renewvless "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/renewvless.sh" && chmod +x /usr/bin/renewvless
-
-
-wget -q -O /usr/bin/renewws "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/renewws.sh" && chmod +x /usr/bin/renewws
-
-
-wget -q -O /usr/bin/cek-mws "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/cek-mws.sh" && chmod +x /usr/bin/cek-mws
-
-wget -q -O /usr/bin/cek-mvs "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/cek-mvs.sh" && chmod +x /usr/bin/cek-mvs
-
-
-wget -q -O /usr/bin/cek-mss "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/cek-mss.sh" && chmod +x /usr/bin/cek-mss
+wget -q -O /usr/bin/delws "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/delws.sh" && chmod +x /usr/bin/delws
 
 
 
-wget -q -O /usr/bin/cek-mts "https://raw.githubusercontent.com/scriswan/premiumsc/main/botol/cek-mts.sh" && chmod +x /usr/bin/cek-mts
+wget -q -O /usr/bin/delss "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/delss.sh" && chmod +x /usr/bin/delss
+
+
+
+wget -q -O /usr/bin/renew-ss "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/renew-ss.sh" && chmod +x /usr/bin/renew-ss
+
+
+wget -q -O /usr/bin/renewtr "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/renewtr.sh" && chmod +x /usr/bin/renewtr
+
+
+wget -q -O /usr/bin/renewvless "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/renewvless.sh" && chmod +x /usr/bin/renewvless
+
+
+wget -q -O /usr/bin/renewws "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/renewws.sh" && chmod +x /usr/bin/renewws
+
+
+wget -q -O /usr/bin/cek-mws "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/cek-mws.sh" && chmod +x /usr/bin/cek-mws
+
+wget -q -O /usr/bin/cek-mvs "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/cek-mvs.sh" && chmod +x /usr/bin/cek-mvs
+
+
+wget -q -O /usr/bin/cek-mss "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/cek-mss.sh" && chmod +x /usr/bin/cek-mss
+
+
+
+wget -q -O /usr/bin/cek-mts "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/botol/cek-mts.sh" && chmod +x /usr/bin/cek-mts
 
 cd /root
 rm -rf kyt.sh
