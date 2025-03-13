7f803f75e06f029119289c5ddfbb4e67252c7036
JESSTUNNEL <143738746+Riswan481@users.noreply.github.com>
2025-03-13 09:32:44 +0700

    Add files via upload

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -9,7 +9,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 apt install rclone -y
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/backup/rclone.conf"
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/scpremium1/jesstunnel/main/backup/rclone.conf"
 cd /bin
 git clone  https://github.com/magnific0/wondershaper.git
 cd wondershaper
@@ -34,11 +34,11 @@ logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/backup/autobackup.sh"
-wget -O backup "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/backup/backup.sh"
-wget -O restore "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/backup/restore.sh"
-wget -O strt "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/backup/strt.sh"
-wget -O limitspeed "https://raw.githubusercontent.com/riswandii-lop/jesstunnel/main/backup/limitspeed.sh"
+wget -O autobackup "https://raw.githubusercontent.com/scpremium1/jesstunnel/main/backup/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/scpremium1/jesstunnel/main/backup/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/scpremium1/jesstunnel/main/backup/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/scpremium1/jesstunnel/main/backup/strt.sh"
+wget -O limitspeed "https://raw.githubusercontent.com/scpremium1/jesstunnel/main/backup/limitspeed.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x restore
